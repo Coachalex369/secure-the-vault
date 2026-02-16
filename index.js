9235cf226e02ef03ec4d28a2ab0b448c43ca353e
Alex Carpenter <carpenteralex78@gmail.com>
2026-02-16 15:32:57 -0700

    secure the vault workshop done

---
+++ index.js
@@ -0,0 +1,14 @@
+let code1;
+code1 = 7 + 3;
+let code2;
+code2 = 100 / 2 - 10;
+let code3;
+code3 = 40 % 1 + 39;
+let message;
+message = "The vault has been secured. The combination is:";
+let codeA;
+codeA = code1 + "-" + code2 + "-" + code3;
+let codeB = `${code1}-${code2}-${code3}`;
+console.log('message');
+console.log('codeA');
+console.log('codeB');
